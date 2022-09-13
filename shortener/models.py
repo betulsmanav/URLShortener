b0c9d02ecd60339b6745fe56b52c66adfdcb772b
betul <betulsmanav@gmail.com>
2022-09-13 21:08:35 +0300

    add

--- shortener/models.py
+++ shortener/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 import uuid
-from django.conf import settings
-# Create your models here.
+from ua_parser import user_agent_parser
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 class Url(models.Model):
     id=models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
@@ -16,25 +16,74 @@ class Url(models.Model):
     def __str__(self):
         return f'{self.user,self.original_url}'
 
-    def short(self):
-        while True:
-            uid=str(uuid.uuid4())[:6]
-            new_url=settings.HOST_URL + "/" + uid
-
-            if not Url.objects.filter(short_url=new_url).exists():
-                break
-        return new_url
-
-    def save(self,*args,**kwargs):
-        if not self.short_url:
-            new_url=self.short()
-            self.short_url=new_url
-        return super().save(*args,**kwargs)
-
     class Meta:
         unique_together = ('original_url', 'short_url')
+   
+
+
+class Log(models.Model):
+    url = models.ForeignKey(Url,on_delete=models.CASCADE, default=None, null=True, editable=False)
+    # create_user = models.ForeignKey(User,on_delete=models.CASCADE, default=None, null=True, editable=False)
+    # timestamp=models.DateTimeField(auto_now_add=True)
+    browser = models.CharField(max_length=100)
+    
+
+        
+
+
+    def gelen(request):
+        # url=
+        # path = request.path
+        # scheme = request.scheme
+        # method = request.method
+        # address = request.META['REMOTE_ADDR']
+        user_agent = request.META['HTTP_USER_AGENT']
+        parsed_string = user_agent_parser.Parse(user_agent)
+        browser=user_agent_parser.ParseUserAgent(user_agent)['family']
+        print("====================",browser)
+        print("+++++++++++",parsed_string)
+
+        log = Log(
+            browser=browser
+        )
+
+        return log
+
+    def save(self,*args, **kwargs):
+        browser=self.gelen()
+        self.browser=browser
+       
+        return super().save(*args, **kwargs)
 
 
 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+# from ua_parser import user_agent_parser
+    # def save(self,*args,**kwargs):
+    #     # user_agent = request.META['HTTP_USER_AGENT']
+    #     # parsed_string = user_agent_parser.PrettyUserAgent(user_agent)
+    #     # self.browser=parsed_string
+        
+    #     return super().save(*args,**kwargs)
+     
 
 
--- shortener/serializers.py
+++ shortener/serializers.py
@@ -1,31 +1,56 @@
 from rest_framework import serializers
-from shortener.models import (Url,)
+from shortener.models import (Url,Log)
 from datetime import datetime
 import uuid
 from django.conf import settings
 
 class UrlSerializer(serializers.ModelSerializer):
+    detail=serializers.HyperlinkedIdentityField(view_name='url-detail')
     user=serializers.StringRelatedField()
-    user_id=serializers.IntegerField(write_only=True,required=False)
     
 
     class Meta:
         model=Url
-        fields=('id','original_url','short_url','user','user_id')
+        fields=('detail','id','original_url','short_url','user','created_at','updated_at')
 
-  
-   
+    def short(self):
+        while True:
+            uid=str(uuid.uuid4())[:6]
+            new_url=settings.HOST_URL + "/" + uid
+
+            if not Url.objects.filter(short_url=new_url).exists():
+                break
+        return new_url
+
     def create(self, validated_data):
+        liste=self.context['request'].data
         original_url=validated_data.pop('original_url')
+        short_url=validated_data.pop('short_url')
         current_user = self.context['request'].user
-        user=Url.objects.create(user=current_user,original_url=original_url)
-        return user
-        
+
+        if type(liste) == list:
+            print("listlistlistlistlist")
+            for q in liste:
+
+                original_url=q["original_url"]
+                if not short_url:
+                        new_url=self.short()
+                url=Url.objects.create(user=current_user,original_url=original_url,short_url=new_url)
+            return url
+
+        elif type(liste) == dict:
+            print("dictdictdictdictdictdictdict")
+            if not short_url:
+                new_url=self.short()
+                url=Url.objects.create(user=current_user,original_url=original_url,short_url=new_url)
+            return url
     
+class LogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Log
+        fields='__all__'
 
-
-
-
+   
 
 
 
